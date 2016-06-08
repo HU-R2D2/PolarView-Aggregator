1d5d1b62ffa847dd4aae8982934a98782447e795
koen de gruijter <koen.degruijter@student.hu.nl>
2016-06-08 11:07:26 +0200

    created a method to translate a polarview

--- source/include/PolarViewAggregator.hpp
+++ source/include/PolarViewAggregator.hpp
@@ -3,12 +3,25 @@
 
 #include "../../../polarview/source/include/MapPolarView.hpp"
 // #include "SensorAggregator.hpp"
+#include "Translation.hpp"
+#include "length.hpp"
+#include "DistanceReading.hpp"
+#include <map>
+#include "Angle.hpp"
+#include <math.h>
 
 namespace r2d2{
     class PolarViewAggregator{
+
+
     public:
         PolarViewAggregator();
         PolarView& aggregate();
+
+
+        std::map<r2d2::Angle, DistanceReading> translate_base_polarview(
+                                    const std::map<r2d2::Angle, DistanceReading> & polarview,
+                                    const r2d2::Translation & position_of_sensor);
     };
 
 }
--- source/src/PolarViewAggregator.cpp
+++ source/src/PolarViewAggregator.cpp
@@ -1,8 +1,49 @@
 #include "../include/PolarViewAggregator.hpp"
+#include <iostream>
 namespace r2d2{
     PolarViewAggregator::PolarViewAggregator(){
     }
 
     // PolarView& PolarViewAggregator::aggregate(){
     // }
+	
+		std::map<r2d2::Angle, DistanceReading> PolarViewAggregator::translate_base_polarview(
+            const std::map<r2d2::Angle, DistanceReading> & polarview,
+            const r2d2::Translation & position_of_sensor){
+
+                std::map<r2d2::Angle, DistanceReading> translated_polarview;
+
+                for (const pair<r2d2::Angle, DistanceReading> & polar_view_iterator : polarview )
+                    if(polar_view_iterator.second.get_result_type() == r2d2::DistanceReading::ResultType::CHECKED){
+
+                        //calculate translation of translated polarpoint
+                        r2d2::Translation PolarPoint(position_of_sensor+r2d2::Translation{
+                                        polar_view_iterator.second.get_length()*cos(polar_view_iterator.first/r2d2::Angle::rad),
+                                        polar_view_iterator.second.get_length()*sin(polar_view_iterator.first/r2d2::Angle::rad),
+                                        0*r2d2::Length::CENTIMETER});
+
+                        std::cout << "debug info: x: " << PolarPoint.get_x()/r2d2::Length::CENTIMETER << ", y: " << PolarPoint.get_y()/r2d2::Length::CENTIMETER << std::endl;
+
+                        //calculate angle of translated polarpoint
+                        r2d2::Angle translated_angle(atan2(PolarPoint.get_y()/Length::CENTIMETER,PolarPoint.get_x()/Length::CENTIMETER)*Angle::rad);
+
+                        //calculate distance of translated polarpoint
+                        r2d2::DistanceReading translated_distance_reading(
+                                    //r2d2::Length(sin(translated_angle/r2d2::Angle::rad)*PolarPoint.get_y()*500),
+                                    r2d2::Length(PolarPoint.get_length()),
+                                    polar_view_iterator.second.get_result_type());
+
+                        //make sure angle is a real number, not flotingpoint
+                        translated_angle = nearbyint(translated_angle/r2d2::Angle::deg)*r2d2::Angle::deg;
+
+                        //save in new (check for duplicates)
+                        translated_polarview.insert(std::pair<r2d2::Angle, DistanceReading>(
+                                    translated_angle,
+                                    translated_distance_reading));
+                    }
+                return translated_polarview;
+            }
+
+
+	
 }
